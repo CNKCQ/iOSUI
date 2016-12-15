f6051ab229ffc8d979896e74459050e3475bce6d
KingCQ <wangchengqvan@hotmail.com>
2016-12-15 15:53:06 +0800

    地图定位开启ATS

--- Maplocation/Maplocation/MapSDK.swift
+++ Maplocation/Maplocation/MapSDK.swift
@@ -9,5 +9,6 @@
 struct MapSDk {
     static func configLocation() {
         AMapServices.shared().apiKey = "fffb25cb54f79a6f28929aa269be6e68"
+        AMapServices.shared().enableHTTPS = true // 从2017年1月1日起，苹果将要求所有提交到 App Store 的应用强制开启 ATS
     }
 }
