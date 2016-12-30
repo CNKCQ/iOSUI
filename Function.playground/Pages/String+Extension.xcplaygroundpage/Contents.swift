cf4f6e16a949ecfca864db902643467db5dcbaba
KingCQ <wangchengqvan@hotmail.com>
2016-12-30 22:27:19 +0800

    update  some method

--- Function.playground/Pages/String+Extension.xcplaygroundpage/Contents.swift
+++ Function.playground/Pages/String+Extension.xcplaygroundpage/Contents.swift
@@ -11,11 +11,31 @@ extension String {
     mutating func substring(to string: String) -> String {
         return substring(to: (range(of: string)?.lowerBound) ?? endIndex)
     }
+    
+    func trimmed(set: CharacterSet = .whitespaces) -> String {
+        return trimmingCharacters(in: set)
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[self.index(self.startIndex, offsetBy: i)])
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = self.index(self.startIndex, offsetBy: r.lowerBound)
+        let end = self.index(self.startIndex, offsetBy: r.upperBound)
+        
+        return self[start...end]
+    }
+
 }
+
 
 var string = "1234<user>"
 
-print(string.remove(after: "<"), string.substring(to: "<"), string.components(separatedBy: "<").first!)
-let text = "ab.c1,23"
-print(text.components(separatedBy: [".", ","]))
-print(["ab", "c1", "23"].reduce("", { $0 + $1}))
+string[2]
+
+string[1..<2]
+//
+//print(string.remove(after: "<"), string.substring(to: "<"), string.components(separatedBy: "<").first!)
+
+
